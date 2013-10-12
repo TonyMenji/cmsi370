13a28d35d1b0edc39c3a1af72d8a21bfb312d785
dondi <dondi@lmu.edu>
2013-10-11 22:07:15 -0700

    Integrate updates from 2013-10-10 class.

---
+++ rpg/rpg.js
@@ -0,0 +1,11 @@
+// Big things have small beginnings...
+$(function () {
+
+    $("#confirm-delete-button").click(function () {
+        console.log("Delete confirmed!!!!!");
+
+        // Now we dismiss the dialog.
+        $('#deleteModal').modal('hide');
+    });
+
+});
