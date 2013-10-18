0ef152e56eb43cb569de71846602db9b8ce28529
amenjiva@lion.lmu.edu <amenjiva@lion.lmu.edu>
2013-10-18 15:44:17 -0700

    RPG Files

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -9,3 +9,50 @@ $(function () {
     });
 
 });
+<<<<<<< HEAD
+=======
+
+$(function () {
+
+    $("#confirm-create-button").click(function () {
+        console.log("Create confirmed!!!!!");
+
+        // Now we dismiss the dialog.
+        $('#createModal').modal('hide');
+    });
+
+});
+
+$(function () {
+
+    $("#confirm-random-button").click(function () {
+        console.log("Randomize confirmed!!!!!");
+
+        // Now we dismiss the dialog.
+        $('#randomModal').modal('hide');
+    });
+
+});
+
+$(function () {
+
+    $("#confirm-edit-button").click(function () {
+        console.log("Randomize confirmed!!!!!");
+
+        // Now we dismiss the dialog.
+        $('#editModal').modal('hide');
+    });
+
+});
+
+$(function () {
+
+    $("#confirm-item-button").click(function () {
+        console.log("Spawn confirmed!!!!!");
+
+        // Now we dismiss the dialog.
+        $('#itemModal').modal('hide');
+    });
+
+});
+>>>>>>> RPG Files
